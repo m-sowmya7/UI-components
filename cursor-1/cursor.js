0984915359ff0409dca1a3ca00874c35b88d13a2
m-sowmya7 <sowmyamls2005@gmail.com>
2025-03-18 19:11:17 +0530

    .

--- cursor-1/cursor.js
+++
@@ -1,8 +0,0 @@
-const circle=document.querySelector("#circle");
-        document.addEventListener("mousemove",(e)=>{
-            let x=e.pageX;
-            let y=e.pageY;
-
-            circle.style.top=y+"px";
-            circle.style.left=x+"px";
-        });
